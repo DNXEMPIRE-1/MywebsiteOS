cde67099767cc88a929ac433115c5c2c0dbab7f5
“Dennis <dennisnzioki019@gmail.com>
2024-04-27 14:42:08 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Desktop from './components/os/Desktop';
+
+function App() {
+    return (
+        <div className="App">
+            <Desktop />
+        </div>
+    );
+}
+
+export default App;
