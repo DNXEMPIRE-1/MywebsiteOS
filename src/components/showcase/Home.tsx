d936051b6ae7cdcb7a89fdc4af126e3b2b9fb614
“Dennis <dennisnzioki019@gmail.com>
2024-05-01 00:59:34 +0300

    Update Home.tsx

--- src/components/showcase/Home.tsx
+++ src/components/showcase/Home.tsx
@@ -18,7 +18,7 @@ const Home: React.FC<HomeProps> = (props) => {
         <div style={styles.page}>
             <div style={styles.header}>
             <img src={dnx} style={styles.Image} alt="" />
-                <h1 style={styles.name}>Dennis Nzioki</h1>
+                <h1 style={styles.name}>Dennis Nzioki DNX</h1>
                 <h2>Software Engineer & UI/UX Designer</h2>
                 <h2>🎨 UI/UX Designer by day, Code Ninja by night. I don't just code; I create magic that users can see and feel. 💻 </h2>
             </div>
