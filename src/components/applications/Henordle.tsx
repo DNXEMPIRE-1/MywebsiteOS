37615e5a28883a65a922f109bd5a11eb8c4f388b
“Dennis <dennisnzioki019@gmail.com>
2024-04-30 19:27:42 +0300

    Create Henordle.tsx

---
+++ src/components/applications/Henordle.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Window from '../os/Window';
+import Wordle from '../wordle/Wordle';
+
+export interface HenordleAppProps extends WindowAppProps {}
+
+const HenordleApp: React.FC<HenordleAppProps> = (props) => {
+    return (
+        <Window
+            top={20}
+            left={300}
+            width={600}
+            height={860}
+            windowBarIcon="windowGameIcon"
+            windowTitle="Henordle"
+            closeWindow={props.onClose}
+            onInteract={props.onInteract}
+            minimizeWindow={props.onMinimize}
+            bottomLeftText={'© Copyright 2024 Dennis Nzioki'}
+        >
+            <div className="site-page">
+                <Wordle />
+            </div>
+        </Window>
+    );
+};
+
+export default HenordleApp;
