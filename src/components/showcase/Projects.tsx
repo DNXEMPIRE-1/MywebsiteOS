0e52472bbb123300046e80454a850724b4a6ed5b
“Dennis <dennisnzioki019@gmail.com>
2024-04-30 19:24:00 +0300

    Create Projects.tsx

---
+++ src/components/showcase/Projects.tsx
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import software from '../../assets/pictures/projects/software.gif';
+import art from '../../assets/pictures/projects/art.gif';
+import music from '../../assets/pictures/projects/music.gif';
+
+export interface ProjectsProps {}
+
+interface ProjectBoxProps {
+    icon: string;
+    title: string;
+    subtitle: string;
+    route: string;
+    iconStyle: React.CSSProperties;
+}
+
+const ProjectBox: React.FC<ProjectBoxProps> = ({
+    icon,
+    title,
+    subtitle,
+    route,
+    iconStyle,
+}) => {
+    const [, setIsHovering] = useState(false);
+    const navigation = useNavigate();
+
+    const handleClick = () => {
+        navigation(`/projects/${route}`);
+    };
+
+    const onMouseEnter = () => {
+        setIsHovering(true);
+    };
+
+    const onMouseLeave = () => {
+        setIsHovering(false);
+    };
+
+    return (
+        <div
+            onMouseDown={handleClick}
+            className="big-button-container"
+            style={styles.projectLink}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+        >
+            <div style={styles.projectLinkLeft}>
+                <img
+                    src={icon}
+                    style={Object.assign(
+                        {},
+                        styles.projectLinkImage,
+                        iconStyle
+                    )}
+                    alt=""
+                />
+                <div style={styles.projectText}>
+                    <h1 style={{ fontSize: 48 }}>{title}</h1>
+                    <h3>{subtitle}</h3>
+                </div>
+            </div>
+            <div style={styles.projectLinkRight}></div>
+        </div>
+    );
+};
+
+const Projects: React.FC<ProjectsProps> = (props) => {
+    return (
+        <div className="site-page-content">
+            <h1>Projects</h1>
+            <h3>& Hobbies</h3>
+            <br />
+            <p>
+                Click on one of the areas below to check out some of my favorite
+                projects I've done in that field. I spent a lot of time to
+                include a lot of visuals and interactive media to showcase each
+                project. Enjoy!
+            </p>
+            <br />
+            <div style={styles.projectLinksContainer}>
+                <ProjectBox
+                    icon={software}
+                    iconStyle={styles.computerIcon}
+                    title="Software"
+                    subtitle="PROJECTS"
+                    route="software"
+                />
+                <ProjectBox
+                    icon={music}
+                    iconStyle={styles.musicIcon}
+                    title="Music"
+                    subtitle="VENTURES"
+                    route="music"
+                />
+                <ProjectBox
+                    icon={art}
+                    iconStyle={styles.artIcon}
+                    title="Art"
+                    subtitle="ENDEAVORS"
+                    route="art"
+                />
+            </div>
+        </div>
+    );
+};
+
+const styles: StyleSheetCSS = {
+    projectLinksContainer: {
+        flexDirection: 'column',
+        width: '100%',
+        display: 'flex',
+        flex: 1,
+    },
+    projectLink: {
+        marginBottom: 24,
+        cursor: 'pointer',
+        width: '100%',
+        boxSizing: 'border-box',
+
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    projectText: {
+        justifyContent: 'center',
+        flexDirection: 'column',
+    },
+    projectLinkImage: {
+        width: 48,
+        // height: 48,
+        marginRight: 38,
+    },
+    projectLinkLeft: {
+        marginLeft: 16,
+        alignItems: 'center',
+    },
+    computerIcon: {
+        width: 56,
+        height: 56,
+    },
+    musicIcon: {
+        width: 48,
+        height: 48,
+    },
+    arrowIcon: {
+        width: 48,
+        height: 48,
+    },
+    artIcon: {
+        width: 21 * 2,
+        height: 37 * 2,
+    },
+};
+
+export default Projects;
