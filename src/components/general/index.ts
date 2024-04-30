2db64125597e9da9775cd0e0e9a720d378dbd3f9
“Dennis <dennisnzioki019@gmail.com>
2024-04-30 19:27:14 +0300

    Create index.ts

---
+++ src/components/general/index.ts
@@ -0,0 +1,6 @@
+import Link from './Link';
+import Icon from './Icon';
+import MusicPlayer from './MusicPlayer';
+import Colors from '../../constants/colors';
+
+export { Icon, Colors, MusicPlayer, Link };
