abbb5ae46c97218bc2cb03e8309d3a7f687d8367
“Dennis <dennisnzioki019@gmail.com>
2024-04-30 19:27:30 +0300

    Create ThisComputer.tsx

---
+++ src/components/applications/ThisComputer.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+// import DesktopShortcut from '../os/DesktopShortcut';
+import Window from '../os/Window';
+
+export interface ThisComputerProps extends WindowAppProps {}
+
+const ThisComputerApp: React.FC<ThisComputerProps> = (props) => {
+    return (
+        <Window
+            top={20}
+            left={20}
+            width={600}
+            height={400}
+            windowBarIcon="computerSmall"
+            windowTitle="This Computer"
+            closeWindow={props.onClose}
+            onInteract={props.onInteract}
+            minimizeWindow={props.onMinimize}
+        >
+            <div className="site-page">
+                <iframe
+                    src="https://oldgoogle.neocities.org/1998/"
+                    title="google"
+                    width="100%"
+                    height="100%"
+                />
+                {/* <div style={}>
+                    <DesktopShortcut
+                        icon="computerBig"
+                        invertText
+                        shortcutName={'Computer Details'}
+                        onOpen={() => {}}
+                    />
+                </div> */}
+            </div>
+        </Window>
+    );
+};
+
+export default ThisComputerApp;
