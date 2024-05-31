12b0f6a4893ae80bee4de8b1a71fbfde54a5d3d7
“Dennis <dennisnzioki019@gmail.com>
2024-05-31 05:01:31 +0300

    Update Projects.tsx

--- src/components/showcase/Projects.tsx
+++ src/components/showcase/Projects.tsx
@@ -102,7 +102,7 @@ const Projects: React.FC<ProjectsProps> = (props) => {
                 />
                 <ProjectBox
                     icon={music}
-                    iconStyle={styles.designIcon}
+                    iconStyle={styles.musicIcon}
                     title="Design"
                     subtitle="CREATIVITY"
                     route="design"
