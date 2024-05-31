0354d21abfcc6f54e2c3672e156933c86f2d07bd
“Dennis <dennisnzioki019@gmail.com>
2024-05-31 04:55:28 +0300

    Update Projects.tsx

--- src/components/showcase/Projects.tsx
+++ src/components/showcase/Projects.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router';
 import software from '../../assets/pictures/projects/software.gif';
 import art from '../../assets/pictures/projects/art.gif';
 import music from '../../assets/pictures/projects/music.gif';
-import design from '../../assets/pictures/projects/design.gif'
+import design from '../../assets/pictures/projects/design.gif';
 
 export interface ProjectsProps {}
 
