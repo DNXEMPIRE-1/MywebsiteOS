d55323c9657b4c4ba652a0548394c3be3aa53782
“Dennis <dennisnzioki019@gmail.com>
2024-06-13 22:00:22 +0300

    Update Software.tsx

--- src/components/showcase/projects/Software.tsx
+++ src/components/showcase/projects/Software.tsx
@@ -30,10 +30,10 @@ const SoftwareProjects: React.FC<SoftwareProjectsProps> = (props) => {
                     dennisnzioki.com is my portfolio website, and also the
                     website you are on right now. This project was an absolute
                     joy to make and challenged me both technically and
-                    creatively. Early in 2024, I knew I wanted to make an
+                    creatively. Early in 2023, I knew I wanted to make an
                     interactive portfolio to aid my job search. I eventually got
                     the idea for this site around early February and began
-                    development early March. I've been developing it alongside
+                    development late July. I've been developing it alongside
                     my last semester at school and if you are reading this, it's
                     pretty much done!
                 </p>
