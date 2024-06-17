baca29477ac6ae1dcbf4bef4266449f58d8f1bc9
“Dennis <dennisnzioki019@gmail.com>
2024-06-17 21:04:00 +0300

    Update Design.tsx

--- src/components/showcase/projects/Design.tsx
+++ src/components/showcase/projects/Design.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import furniture from '../../../assets/pictures/projects/design/furniture.png';
 import ecobazar from '../../../assets/pictures/projects/design/ecobazar.png';
+import task from '../../../assets/pictures/projects/design/task.png';
 
 import gsts from '../../../assets/pictures/projects/art/gsts.png';
 
