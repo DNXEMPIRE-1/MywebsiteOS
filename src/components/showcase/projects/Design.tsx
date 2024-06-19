766ba93ccc2634b364d196efad2e9453bcf6241e
“Dennis <dennisnzioki019@gmail.com>
2024-06-19 17:13:43 +0300

    Update Design.tsx

--- src/components/showcase/projects/Design.tsx
+++ src/components/showcase/projects/Design.tsx
@@ -5,6 +5,7 @@ import ecobazar from '../../../assets/pictures/projects/design/ecobazar.png';
 import task from '../../../assets/pictures/projects/design/task.png';
 import prada from '../../../assets/pictures/projects/design/prada.png';
 import sneaker from '../../../assets/pictures/projects/design/sneaker.png';
+import coffee from '../../../assets/pictures/projects/design/coffee.png';
 
 import gsts from '../../../assets/pictures/projects/art/gsts.png';
 
