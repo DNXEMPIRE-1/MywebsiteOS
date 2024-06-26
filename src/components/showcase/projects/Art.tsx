c73a4abb34d97686805ccb9143fee1eea3693ccd
“Dennis <dennisnzioki019@gmail.com>
2024-06-26 13:43:50 +0300

    Update Art.tsx

--- src/components/showcase/projects/Art.tsx
+++ src/components/showcase/projects/Art.tsx
@@ -28,7 +28,7 @@ const ArtProjects: React.FC<ArtProjectsProps> = (props) => {
                 <h2>Pixel Art and Animation</h2>
                 <br />
                 <p>
-                    In high school, I started learning how to draw and animate
+                    In University, I started learning how to draw and animate
                     pixel art after becoming interested in video game
                     development. As I worked on making games over the years, I
                     learned so much about pixel art and fell in love with
@@ -46,9 +46,7 @@ const ArtProjects: React.FC<ArtProjectsProps> = (props) => {
                 </div>
                 <p>
                     I really started enjoying the work I was doing, and felt I
-                    was improving a lot. Around 2017, I started to upload videos
-                    of my work flow and animation process to YouTube just for
-                    fun. I was really surprised however when my first video took
+                    was improving a lot.. I was really surprised however when my first video took
                     off and got a few thousand views! Below are the video links
                     to the work I uploaded to youtube.
                 </p>
