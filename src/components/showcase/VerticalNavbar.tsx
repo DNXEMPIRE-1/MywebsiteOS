a1298e8dfdc8152ec8f1ef6cecd9f72231a853b7
“Dennis <dennisnzioki019@gmail.com>
2024-06-29 11:15:13 +0300

    Update VerticalNavbar.tsx

--- src/components/showcase/VerticalNavbar.tsx
+++ src/components/showcase/VerticalNavbar.tsx
@@ -70,7 +70,7 @@ const VerticalNavbar: React.FC<VerticalNavbarProps> = (props) => {
                             <Link
                                 containerStyle={styles.insetLink}
                                 to="projects/art"
-                                text="ART & Animation "
+                                text="ART & A "
                             />
                             <Link
                                 containerStyle={styles.insetLink}
