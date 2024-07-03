b2423f5c64bb00077bdc9e80ad771e89244567a2
“Dennis <dennisnzioki019@gmail.com>
2024-07-03 21:08:58 +0300

    Update education.tsx

--- src/components/showcase/education.tsx
+++ src/components/showcase/education.tsx
@@ -172,7 +172,7 @@ const Experience: React.FC<ExperienceProps> = (props) => {
             <div className="text-block">
                 <p>
                     The San Damiano Boarding Primary School is an ordinary
-                    private educational institute in Kyanzavi, M Kenya.
+                    private educational institute in Kyanzavi, Kenya.
                     This school is managed / sponsored by religious organization
                     and offers Kenya Certificate of Primary Education (KCPE),
                     which is controlled by the Kenya National Examination
