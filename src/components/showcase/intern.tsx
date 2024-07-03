e1ed0e65e01468227c7457889d87d7da7f2f6e15
“Dennis <dennisnzioki019@gmail.com>
2024-07-03 20:08:56 +0300

    Update intern.tsx

--- src/components/showcase/intern.tsx
+++ src/components/showcase/intern.tsx
@@ -29,13 +29,12 @@ const Experience: React.FC<ExperienceProps> = (props) => {
             </div>
             <div className="text-block">
                 <p>
-                    StudyBuddy is an innovative e-learning platform
-                    revolutionizing education in Africa by providing students
-                    with access to a diverse range of teachers from any school
-                    in the country. Through StudyBuddy, students can engage with
-                    teachers outside their own school, expanding their learning
-                    opportunities and gaining insights from a variety of
-                    perspectives.
+                    National Bank of Kenya Limited is a financial services
+                    provider headquartered in Nairobi, Kenya. It is licensed as
+                    a commercial bank, by the Central Bank of Kenya, the
+                    national banking regulator. The bank has also been running
+                    Agency banking model. National Bank is a subsidiary of KCB
+                    Group Plc after a successful acquisition in 2019.
                 </p>
                 <br />
                 <ul>
