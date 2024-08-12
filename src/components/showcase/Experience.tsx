d4e525fa74f4466fe2083e0ac627ce9ad3aeba71
Dennis Nzioki <dennisnzioki019@gmail.com>
2024-08-12 09:28:45 +0300

    Update Experience.tsx

--- src/components/showcase/Experience.tsx
+++ src/components/showcase/Experience.tsx
@@ -29,7 +29,12 @@ const Experience: React.FC<ExperienceProps> = (props) => {
             </div>
             <div className="text-block">
                 <p>
-                   
+                National Bank of Kenya Limited is a financial services
+                    provider headquartered in Nairobi, Kenya. It is licensed as
+                    a commercial bank, by the Central Bank of Kenya, the
+                    national banking regulator. The bank has also been running
+                    Agency banking model. National Bank is a subsidiary of KCB
+                    Group Plc after a successful acquisition in 2019.
                 </p>
                 <br />
                 <ul>
