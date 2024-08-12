6105868b9ad7069e54391cf4b85a3c2872d3ba32
Dennis Nzioki <dennisnzioki019@gmail.com>
2024-08-12 12:00:52 +0300

    Update intern.tsx

--- src/components/showcase/intern.tsx
+++ src/components/showcase/intern.tsx
@@ -117,7 +117,11 @@ const Experience: React.FC<ExperienceProps> = (props) => {
                         </p>
                     </li>
                     <li>
-                        <p>Integrated RESTful APIs and GraphQL endpoints into Vue.js applications to fetch and manage data efficiently.</p>
+                        <p>
+                            Integrated RESTful APIs and GraphQL endpoints into
+                            Vue.js applications to fetch and manage data
+                            efficiently.
+                        </p>
                     </li>
                     <li>
                         <p></p>
