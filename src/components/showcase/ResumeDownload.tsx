0f45088368aaaa350c84b62350dcdf45c9f26523
DNXEMPIRE-1 <89406534+DNXEMPIRE-1@users.noreply.github.com>
2024-10-20 22:27:47 +0300

    Update ResumeDownload.tsx

--- src/components/showcase/ResumeDownload.tsx
+++ src/components/showcase/ResumeDownload.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import printer from '../../assets/resume/printer.gif';
-import Resume from '../../assets/resume/Dennis_Nzioki_CV (1).pdf';
+import Resume from '../../assets/resume/DennisNziokiCVM.pdf';
 
 export interface ResumeDownloadProps {
     altText?: string;
